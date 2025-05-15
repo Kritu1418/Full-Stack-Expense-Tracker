6f590b3afa4903e3ff7f724b0857c0a348350222
Kritu1418 <jhakritika957@gmail.com>
2025-05-15 23:01:34 +0530

    Add expense tracker project files

---
+++ routes/userRoute.js
@@ -0,0 +1,17 @@
+
+
+const express=require('express');
+const { loginController, registerController } = require('../controllers/userController');
+const router=express.Router();
+// Routers
+
+// POST||LOGIN USER
+router.post('/login',loginController)
+
+// POST||REGISTER USER
+router.post('/register',registerController)
+
+
+
+
+module.exports=router;
